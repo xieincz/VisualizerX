bca09de32770775208fa23cf5003508c0dcfd460
CHEN Zhikai <32872030+chenzhik@users.noreply.github.com>
2022-08-04 18:25:51 +0800

    Add files via upload

---
+++ visualizer/visualizerX.py
@@ -0,0 +1,57 @@
+from bytecode import Bytecode, Instr
+
+class get_local(object):
+    cache = {}
+    is_activate = False
+
+    def __init__(self, *varnames):
+        """varname: turple"""
+        self.varnames = varnames
+
+    def __call__(self, func):
+        if not type(self).is_activate:
+            return func
+        
+        c = Bytecode.from_code(func.__code__)
+        
+        # store return variable
+        extra_code = [ Instr('STORE_FAST', '_res')]
+        
+        # store local variables
+        for var_name in self.varnames:
+            type(self).cache[func.__qualname__ + '.' + var_name] = []  # create cache
+            extra_code.extend([Instr('LOAD_FAST', var_name), Instr('STORE_FAST', var_name + '_value')])
+        
+        # push to TOS
+        extra_code.extend([Instr('LOAD_FAST', '_res')])
+        
+        for var_name in self.varnames:
+            extra_code.extend([Instr('LOAD_FAST', var_name + '_value')])
+            
+        extra_code.extend([
+                Instr('BUILD_TUPLE', 1 + len(self.varnames)),
+                Instr('STORE_FAST', '_result_tuple'),
+                Instr('LOAD_FAST', '_result_tuple')
+            ])
+            
+        c[-1:-1] = extra_code
+        func.__code__ = c.to_code()
+        
+        # callback
+        def wrapper(*args, **kwargs):
+            res, *values = func(*args, **kwargs)
+            for var_idx in range(len(self.varnames)):
+                value = values[var_idx].detach().cpu().numpy()
+                type(self).cache[func.__qualname__ + '.' + self.varnames[var_idx]].append(value)
+            return res
+        
+        return wrapper
+
+    @classmethod
+    def clear(cls):
+        for key in cls.cache.keys():
+            cls.cache[key] = []
+
+    @classmethod
+    def activate(cls):
+        cls.is_activate = True
